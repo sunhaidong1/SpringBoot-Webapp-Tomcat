5e0573bdfe0af891af82105180cd1852251da9d2
sunhaidong1 <112157017+sunhaidong1@users.noreply.github.com>
2024-01-22 14:48:51 +0800

    Update HomeController.java

--- src/main/java/com/myapp/controller/HomeController.java
+++ src/main/java/com/myapp/controller/HomeController.java
@@ -9,4 +9,14 @@ public class HomeController {
     public String index() {
         return "index";
     }
+    @GetMapping("/get_ip")
+    public String getIp(HttpServletRequest request) {
+        String xForwardedForHeader = request.getHeader("X-Forwarded-For");
+        return "X-Forwarded-For:" + xForwardedForHeader;
+    }
+
+    @GetMapping("/headers")
+    public Map<String, String> getHeaders(@RequestHeader Map<String, String> headers) {
+        return headers;
+    }
 }
